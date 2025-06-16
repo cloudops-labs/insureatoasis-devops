3fd0a8b0308609391184170cf664a7ef37f65ca3
pruthvirajsuradkar <41281406+pruthvirajsuradkar@users.noreply.github.com>
2025-06-16 15:09:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,21 +5,24 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "demo-vpc"
+    Name = "insureatoasis-vpc"
   }
 }
 
-resource "aws_eks_cluster" "k8s_cluster" {
-  name     = "demo-eks-cluster"
-  role_arn = var.eks_role_arn
+resource "aws_eks_cluster" "demo" {
+  name     = "insureatoasis-eks"
+  role_arn = "arn:aws:iam::979285759387:role/eks-cluster-role"
 
   vpc_config {
-    subnet_ids = var.subnet_ids
+    subnet_ids = [
+      "subnet-12345678",
+      "subnet-23456789"
+    ]
   }
 
   depends_on = [aws_vpc.main]
 }
 
 output "cluster_endpoint" {
-  value = aws_eks_cluster.k8s_cluster.endpoint
+  value = aws_eks_cluster.demo.endpoint
 }
